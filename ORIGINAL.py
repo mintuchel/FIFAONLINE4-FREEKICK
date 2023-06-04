13d2261208fc013650a8bb192dcaeacdfeafda03
mintuchel <max3627@naver.com>
2023-06-05 00:12:40 +0900

    dev: Setting the field and the goal post

--- ORIGINAL.py
+++ ORIGINAL.py
@@ -1,3 +1,17 @@
 from vpython import *
 
-# FIFAONLINE4-FREEKICK START
+import random
+
+ground = box(pos=vec(0,0,0),size=vec(70,0.1,70),color=color.green)
+
+# box center = mid position -> if height changes pos.y have to change
+post1 = box(pos=vec(-4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red)
+post2 = box(pos=vec(4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red)
+post3 = box(pos=vec(0,4,-20),axis = vec(1,0,0),size=vec(8.5,0.5,2),color=color.red)
+
+# post center 정사영
+post2dpos = vec(0,0,-20)
+
+ball = sphere(pos=vec(random.randint(-15,15),0.2,random.randint(8,15)),radius=0.2,color=color.white)
+ball.m = 1
+ball.r = 0.2
