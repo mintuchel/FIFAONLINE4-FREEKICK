52e3dc038eb3a74bea647f855f7e3c8f94176d0e
mintuchel <max3627@naver.com>
2023-06-05 00:58:00 +0900

    feat: Update camera control functionality
    - update to only respond to 'left' and 'right' keys
    - rotate by 1 radian per keystroke

--- ORIGINAL.py
+++ ORIGINAL.py
@@ -1,9 +1,10 @@
 from vpython import *
-
+ 
 import random
 
 scene = canvas(width = 1400, height = 600, title = "피파온라인4")
 scene.background = color.white
+
 
 def shootbtn(b) :
     b.disabled=True
@@ -16,48 +17,47 @@ btnShoot = button(text="shoot",bind=shootbtn)
 ground = box(pos=vec(0,0,0),size=vec(50,0.1,50),color=color.green)
 
 # box center = mid position -> if height changes pos.y have to change
-post1 = box(pos=vec(-4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red)
-post2 = box(pos=vec(4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red)
-post3 = box(pos=vec(0,4,-20),axis = vec(1,0,0),size=vec(8.5,0.5,2),color=color.red)
+post1 = box(pos=vec(-4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red) # left post
+post2 = box(pos=vec(4,2,-20),axis=vec(1,0,0),size=vec(0.5,4,2),color=color.red) # right post
+post3 = box(pos=vec(0,4.25,-20),axis = vec(1,0,0),size=vec(8.5,0.5,2),color=color.red) # top post
 
 # post center 정사영
 post2dpos = vec(0,0,-20)
 
-ball = sphere(pos=vec(random.randint(-15,15),0.2,random.randint(8,15)),radius=0.2,color=color.white)
+ball = sphere(pos=vec(random.randint(-15,15),0.3,random.randint(8,15)),radius=0.3,color=color.white)
 ball.m = 1
 ball.r = 0.2
 
-ball2dpos = ball.pos - vec(0,0.2,0)
+# ball pos 정사영
+ball2dpos = ball.pos - vec(0,0.3,0)
 
 eyedir = hat(ball2dpos-post2dpos)
 eye2dpos = vec(ball.pos.x/(ball.pos.z+20)*(18+20),0,18)
 eyepos = eye2dpos + vec(0,2,0)
 
-shootdir = arrow(pos=ball2dpos,axis=6*hat(ball2dpos-eye2dpos),shaftwidth=0.2,color=color.blue)
+shootdir = arrow(pos=ball2dpos,axis=4*hat(ball2dpos-eye2dpos),shaftwidth = 0.15,color=color.black)
 
 scene.camera.pos = eyepos
+scene.camera.axis = shootdir.axis
 
+
+# update camera control to only respond to 'left' and 'right' keystroke
+angle = radians(1)
+axis = vec(0,1,0)
+origin = ball.pos
 
 while !btnShoot.disabled :
     rate(100)
-
+        
     s = keysdown()
     print("You pressed the key", s)  
-
-    dx = .1; dz = 0.02   # easily-changeable values
-
+    
     if 'left' in s: 
-        scene.camera.pos += vec(-dx,0,-dz)#X AXIS
-        shootdir.axis=6*hat(shootdir.axis+vec(-dx,0,-dz))
+        scene.camera.rotate(angle=radians(-1), axis=axis, origin=origin)
+        shootdir.axis=6*hat(scene.camera.axis)
     if 'right' in s: 
-        scene.camera.pos += vector(dx,0,-dz)
-        shootdir.axis=6*hat(shootdir.axis+vec(dx,0,dz))
-    if 'up' in s: 
-        scene.camera.pos += vec(0,0,-dz) #Y AXIS
-        shootdir.axis= 6*hat(shootdir.axis+vec(0,0,-dz*5))
-    if 'down' in s: 
-        scene.camera.pos += vec(0,0,dz)
-        shootdir.axis = 6*hat(shootdir.axis+vec(0,0,dz*5))
+        scene.camera.rotate(angle=radians(1), axis=axis, origin=origin)
+        shootdir.axis=6*hat(scene.camera.axis)
 
 # the most important variable
 # used in final shooting
@@ -73,14 +73,14 @@ scene.waitfor('keydown')
 
 while True:
     rate(100)  # Limit the loop rate for smooth animation
-
+    
     s = keysdown()  # Get the keys that are currently pressed
-
+    
     print(s)
     if 'z' in s and 'right' in s :
         right_zd += dpower
         print("right zd Power :", right_zd)
-    else if 'z' in s and 'left' in s :
+    else if  'z' in s and 'left' in s :
         left_zd += dpower
         print("left zd Power :", left_zd)
     else if 'q' in s and 'd' in s :
